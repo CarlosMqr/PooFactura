2e7dfef06cd3915245c06fa2bd6990ec5a681805
Carlos Mendoza <Carlos.Mendoza@gmail.com>
2023-07-09 20:08:36 -0600

    se agreo constructor a la clase Cliente

--- src/org/cmendoza/poo/factura/modelo/Cliente.java
+++ src/org/cmendoza/poo/factura/modelo/Cliente.java
@@ -2,9 +2,13 @@ package org.cmendoza.poo.factura.modelo;
 
 public class Cliente {
     private String nombre;
-    private int Nocuenta;
+    private int noCuenta;
 
     /////////// CONSTRUCTOR ///////////////
+    public Cliente(int noCuenta , String nombre){
+        this.noCuenta = noCuenta;
+        this.nombre = nombre;
+    }
     ////////// GETTER AND SETTER /////////
     ///////// MÉTODOS ///////////////////
 }
