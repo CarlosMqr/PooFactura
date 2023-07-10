53bfed8dd29f7f20a5f69b0f01a0f9dc13e30f15
Carlos Mendoza <Carlos.Mendoza@gmail.com>
2023-07-09 19:50:12 -0600

    se agreagron atributos para la aclase Cliente

--- src/org/cmendoza/poo/factura/modelo/Cliente.java
+++ src/org/cmendoza/poo/factura/modelo/Cliente.java
@@ -1,6 +1,9 @@
 package org.cmendoza.poo.factura.modelo;
 
 public class Cliente {
+    private String nombre;
+    private int Nocuenta;
+
     /////////// CONSTRUCTOR ///////////////
     ////////// GETTER AND SETTER /////////
     ///////// MÉTODOS ///////////////////
