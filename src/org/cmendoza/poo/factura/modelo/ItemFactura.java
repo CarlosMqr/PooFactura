6b1f2cd4af6167efdc05fb6c61377f7740b70ef4
Carlos Mendoza <Carlos.Mendoza@gmail.com>
2023-07-09 19:56:12 -0600

    se agregaron atributos a la clase ItemFactura

--- src/org/cmendoza/poo/factura/modelo/ItemFactura.java
+++ src/org/cmendoza/poo/factura/modelo/ItemFactura.java
@@ -1,6 +1,8 @@
 package org.cmendoza.poo.factura.modelo;
 
 public class ItemFactura {
+    private Producto producto;
+    private int cantidad;
     /////////// CONSTRUCTOR ///////////////
     ////////// GETTER AND SETTER /////////
     ///////// MÉTODOS ///////////////////
