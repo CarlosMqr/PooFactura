a52b616c3c154a44d2b2bf97d91fbaa2b9a498e2
Carlos Mendoza <Carlos.Mendoza@gmail.com>
2023-07-09 19:52:31 -0600

    se agregaron atributos para la clase Producto

--- src/org/cmendoza/poo/factura/modelo/Producto.java
+++ src/org/cmendoza/poo/factura/modelo/Producto.java
@@ -1,6 +1,11 @@
 package org.cmendoza.poo.factura.modelo;
 
 public class Producto {
+    private int idProducto;
+    private int idProductoU;
+    private String nombre;
+    private int precio;
+
     /////////// CONSTRUCTOR ///////////////
     ////////// GETTER AND SETTER /////////
     ///////// MÉTODOS ///////////////////
