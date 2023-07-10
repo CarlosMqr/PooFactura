cdee20c964e0f9e9a66d3fe3f5866607a6e5825c
Carlos Mendoza <Carlos.Mendoza@gmail.com>
2023-07-09 20:38:27 -0600

    se agregaron getter ans setter a la clase Cliente

--- src/org/cmendoza/poo/factura/modelo/Cliente.java
+++ src/org/cmendoza/poo/factura/modelo/Cliente.java
@@ -10,5 +10,14 @@ public class Cliente {
         this.nombre = nombre;
     }
     ////////// GETTER AND SETTER /////////
+    public String getNombre(){
+        return this.nombre;
+    }
+    public int getNoCuenta(){
+        return this.noCuenta;
+    }
+    public void setNoCuenta(int noCuenta){
+        this.noCuenta = noCuenta;
+    }
     ///////// MÉTODOS ///////////////////
 }
