11fb84595311b4cb89b9a732374763da10ffa314
Carlos Mendoza <Carlos.Mendoza@gmail.com>
2023-07-09 20:06:07 -0600

    se agreo constructor a la clase ItemFactura

--- src/org/cmendoza/poo/factura/modelo/ItemFactura.java
+++ src/org/cmendoza/poo/factura/modelo/ItemFactura.java
@@ -4,6 +4,10 @@ public class ItemFactura {
     private Producto producto;
     private int cantidad;
     /////////// CONSTRUCTOR ///////////////
+    public ItemFactura(Producto producto, int cantidad){
+        this.producto=producto;
+        this.cantidad =cantidad;
+    }
     ////////// GETTER AND SETTER /////////
     ///////// MÉTODOS ///////////////////
 }
