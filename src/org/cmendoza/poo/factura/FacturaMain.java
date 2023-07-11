9f6054d7d7ab93f2f3ed94114f318bcaff7c3e7a
Carlos Mendoza <Carlos.Mendoza@gmail.com>
2023-07-11 14:55:22 -0600

    se agrego proceso de factura

--- src/org/cmendoza/poo/factura/FacturaMain.java
+++ src/org/cmendoza/poo/factura/FacturaMain.java
@@ -2,6 +2,7 @@ package org.cmendoza.poo.factura;
 
 import org.cmendoza.poo.factura.modelo.Cliente;
 import org.cmendoza.poo.factura.modelo.Factura;
+import org.cmendoza.poo.factura.modelo.ItemFactura;
 import org.cmendoza.poo.factura.modelo.Producto;
 
 import java.util.Scanner;
@@ -19,7 +20,23 @@ public class FacturaMain {
         float precio;
         int cantidad;
 
+        for (int i =0; i < 5; i++){
+        producto = new Producto();
+            System.out.println("nombre del producto" + producto.getIdProducto() + ": ");
+            nombrePrd = s.nextLine();
+            producto.setNombre(nombrePrd);
 
+            System.out.println("Precio: ");
+            precio = s.nextFloat();
+
+            System.out.println("cantidad: ");
+            cantidad = s.nextInt();
+
+            ItemFactura f = new ItemFactura(producto,cantidad);//constructor de la clase ItemFactura
+            facCarlos.addItemFactura(f);//metodo de la clase factura
+            System.out.println();
+        }
+        System.out.println(facCarlos.verDetalle());
 
 
 
