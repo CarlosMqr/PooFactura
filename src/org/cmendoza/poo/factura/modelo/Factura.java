3c362fe635a6c4a64013d2c062e3f0312d06f536
Carlos Mendoza <Carlos.Mendoza@gmail.com>
2023-07-11 14:44:17 -0600

    se agrego metodo ver detalle a la clase Factura

--- src/org/cmendoza/poo/factura/modelo/Factura.java
+++ src/org/cmendoza/poo/factura/modelo/Factura.java
@@ -43,15 +43,39 @@ public class Factura {
         }
     }
     ///////// MÉTODOS ///////////////////
-    public float clacularTotal(){
+    public float calcularTotal(){
             float total = 0.0f;
         for (ItemFactura it: this.facturas) {
             if (it == null){
                 continue;
             }
-
+            total += it.calcularImporte();
         }
         return total;
+    }
+
+    public String verDetalle(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("\nID Factura: " + this.getIdFactura());
+        sb.append("\nCliente: " + cliente.getNombre());
+        sb.append("\nNumero cliente: " + cliente.getNoCuenta());
+        sb.append("\nDescripcion: " + this.descripcion);
+        sb.append("\nEmicion de Factura: " + this.fecha);
+        sb.append("\n#\tNombre\t$\tCant.\tTotal\n");
+        for (ItemFactura fp: this.facturas) {
+            if (fp == null){
+                continue;
+            }
+            sb.append(fp.getProducto().getIdProducto());
+            sb.append("\tProducto: " + fp.getProducto().getNombre());
+            sb.append("\tPrecio:" + fp.getProducto().getPrecio());
+            sb.append("\tCantidad: " + fp.getCantidad());
+            sb.append("\tTotal: " + fp.calcularImporte());
+
+        }
+        sb.append("Total de la compra: " + calcularTotal());
+
+        return toString();
     }
 
 
